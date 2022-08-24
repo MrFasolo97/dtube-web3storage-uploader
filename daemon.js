87e9a6e2d5192556331ad9d2de178d22cfa2b42b
fasolo97 <79fasolo97@gmail.com>
2022-08-24 15:48:51 +0200

    Upgraded logger, added eslint, refactored most of the code, fixed null bug, now returning version and app name on root path.

--- daemon.js
+++ daemon.js
@@ -1,80 +1,102 @@
-import process from 'process'
-import { Command } from 'commander'
-import { Web3Storage, getFilesFromPath } from 'web3.storage'
-import express from 'express'
-import multer from 'multer'
-import * as fs from 'fs'
+import process, { exit } from 'process';
+import { Command } from 'commander';
+import { Web3Storage, getFilesFromPath } from 'web3.storage';
+import express from 'express';
+import multer from 'multer';
+import * as fs from 'fs';
+import version from 'project-version';
+import log4js from 'log4js';
 
-let filesUploaded = {}
+log4js.configure({
+  appenders: {
+    logs: { type: 'file', filename: 'logs/logs.log' },
+    console: { type: 'console' },
+  },
+  categories: {
+    logs: { appenders: ['logs'], level: 'trace' },
+    console: { appenders: ['console'], level: 'trace' },
+    default: { appenders: ['console', 'logs'], level: 'trace' },
+  },
+});
 
-const app = express()
-const upload = multer({ dest: 'tmp/' })
+const program = new Command();
+program
+  .option('-t, --token <token>', 'API Token')
+  .option('-d, --daemon', 'Run as daemon', false)
+  .option('-c, --config <file>', 'Config file', './config.json');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const logger = log4js.getLogger();
 
+program.parse(process.argv);
+const opts = program.opts();
+let configJSON = {};
 
-app.post('/uploadVideo', upload.single("video"), (req, res, next) => {
-    let file = req.file
-    if (typeof file !== 'undefined') {
-        let file_name = file.filename
-        filesUploaded[file_name] = {}
-        filesUploaded[file_name]["progress"] = "incomplete"
-        uploadFile(web3token, file_name)
-        res.send({"status": "ok", "token": file_name})
-    } else {
-        res.send({"status": "error", "error": "file parameter not specified!"})
-    }
-})
-
-
-app.get('/progress/:token', (req, res, next) => {
-    if (filesUploaded[req.params.token] !== null) {
-        res.send(filesUploaded[req.params.token])
-        if (filesUploaded[req.params.token].progress == "complete") {
-            delete filesUploaded[req.params.token]
-        }
-    } else {
-        res.send({"status": "error", "error": "Token not found."})
-    }
-})
-
-
-async function uploadFile (web3token, file_name) {
-    let file_path = 'tmp/' + file_name
-    if (!web3token) {
-        return console.error('A token is needed. You can create one on https://web3.storage')
-    }
-    console.log(file_path)
-    const storage = new Web3Storage({ token: web3token })
-    const file = await getFilesFromPath(file_path)
-    const cid = await storage.put(file, { wrapWithDirectory: false })
-    console.log('Content added with CID:', cid)
-    fs.unlink(file_path, function (err) {
-        if (err) throw err
-        // if no error, file has been deleted successfully
-        console.log('File '+file_path+' deleted!')
-    })
-    filesUploaded[file_name]['cid'] = cid
-    filesUploaded[file_name]['progress'] = 'complete'
-    return cid
+if (fs.existsSync(opts.config)) {
+  configJSON = JSON.parse(fs.readFileSync(opts.config));
+} else {
+  logger.fatal('Config file not found!');
+  exit();
 }
 
-const program = new Command()
-program
-    .option('-t, --token <token>', 'API Token')
-    .option('-d, --daemon', 'Run as daemon', false)
-    .option('-c, --config <file>', 'Config file', './config.json')
+const web3token = opts.token || configJSON.web3token;
+const port = configJSON.port || 5000;
 
-program.parse(process.argv)
-let opts = program.opts()
-let configJSON = JSON.parse(fs.readFileSync(opts.config))
-const web3token = opts.token || configJSON.web3token
-const port = configJSON.port || 5000
+const filesUploaded = {};
+
+const app = express();
+const upload = multer({ dest: 'tmp/' });
 
 if (opts.daemon) {
-    app.listen(port, () => {
-        console.log(`Listening on port ${port}`)
-    })
+  app.listen(port, () => {
+    logger.info(`Listening on port ${port}`);
+  });
 }
+
+async function uploadFile(web3token2, fileName, uploadedBy) {
+  const filePath = `tmp/${fileName}`;
+  if (!web3token2) {
+    return logger.error('A token is needed. You can create one on https://web3.storage');
+  }
+  logger.info(`[ ${uploadedBy} ] ${filePath}`);
+  const storage = new Web3Storage({ token: web3token2 });
+  const file = await getFilesFromPath(filePath);
+  const cid = await storage.put(file, { wrapWithDirectory: false });
+  logger.info(`[ ${uploadedBy} ] Content added with CID: ${cid}`);
+  fs.unlink(filePath, (err) => {
+    if (err) throw err;
+    // if no error, file has been deleted successfully
+    logger.info(`File ${filePath} deleted!`);
+  });
+  filesUploaded[fileName].cid = cid;
+  filesUploaded[fileName].progress = 'complete';
+  return cid;
+}
+
+app.get('/', (req, res) => {
+  res.send({ version, app: 'dtube-web3storage-uploader' });
+});
+
+app.post('/uploadVideo', upload.single('video'), (req, res) => {
+  const { file } = req;
+  if (typeof file !== 'undefined') {
+    const fileName = file.filename;
+    filesUploaded[fileName] = {};
+    filesUploaded[fileName].progress = 'incomplete';
+    filesUploaded[fileName].status = 'ok';
+    uploadFile(web3token, fileName, req.headers['x-forwarded-for']);
+    res.send({ status: 'ok', token: fileName });
+  } else {
+    res.send({ status: 'error', error: 'file parameter not specified!' });
+  }
+});
+
+app.get('/progress/:token', (req, res) => {
+  if (filesUploaded[req.params.token] !== null) {
+    res.send(filesUploaded[req.params.token]);
+    if (filesUploaded[req.params.token].progress === 'complete') {
+      delete filesUploaded[req.params.token];
+    }
+  } else {
+    res.send({ status: 'error', error: 'Token not found.' });
+  }
+});
