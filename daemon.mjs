7cba8aba155ea92dd2c7bf311e38667efa81d73f
fasolo97 <79fasolo97@gmail.com>
2023-03-03 14:16:31 +0100

    Splitted auth and API

--- daemon.mjs
+++ daemon.mjs
@@ -1,28 +1,13 @@
 import process, { exit } from 'process';
 import { Command } from 'commander';
-import https from 'https';
-import mime from 'mime';
 import express from 'express';
 import * as fs from 'fs';
 import log4js from 'log4js';
 import sanitize from 'sanitize-filename';
-import tus from 'tus-node-server';
 import version from 'project-version';
-import tusMetadata from 'tus-metadata';
-import javalon from './javalon.js';
+import { randomInt } from 'crypto';
 
 let configJSON = {};
-const { EVENTS } = tus;
-const tusServer = new tus.Server(
-  {
-    path: '/',
-  },
-);
-
-tusServer.datastore = new tus.FileStore({
-  path: '/download',
-  directory: './files',
-});
 
 const filesUploaded = {};
 
@@ -53,8 +38,8 @@ const opts = program.opts();
 
 if (fs.existsSync(opts.config)) {
   configJSON = JSON.parse(fs.readFileSync(opts.config));
-  if (configJSON.storage_provider == null) {
-    logger.fatal('Storage provider must be set!');
+  if (configJSON.storage_providers == null) {
+    logger.fatal('Storage provider(s) list must be set!');
     exit();
   }
 } else {
@@ -64,163 +49,130 @@ if (fs.existsSync(opts.config)) {
 
 const port = configJSON.port || 5000;
 
-const { default: storageStore } = await import(`./storage_providers/${configJSON.storage_provider}.mjs`);
+const storageStore = [];
+
+function loadStoragePlugins(configJSONRef) {
+  for (let i = 0; i < configJSONRef.storage_providers.length; i += 1) {
+    import(`./storage_providers/${configJSONRef.storage_providers[i]}.mjs`).then((res) => {
+      storageStore.push({ store: res.default });
+    });
+  }
+}
+
+loadStoragePlugins(configJSON);
 
 // Takes 3 parameters:
 // a config Object, file's name as string and string defining the original uploader.
-async function uploadFile(configJSONRef, fileName, uploadedBy) {
-  const cid = await storageStore(configJSONRef, logger, fileName, uploadedBy);
-  filesUploaded[fileName].cid = cid;
-  filesUploaded[fileName].progress = 'uploaded';
-  return cid;
+async function uploadFile(configJSONRef, fileId, uploadedBy, cb) {
+  let ret;
+  const fileName = sanitize(fileId);
+  logger.info(storageStore);
+  logger.info(`Uploading file ${fileName} for user ${uploadedBy}`);
+  if (typeof storageStore === 'object') {
+    for (let i = 0; i < storageStore.length; i += 1) {
+      if (typeof storageStore[i] === 'object') {
+        if (typeof storageStore[i].store === 'function') {
+          storageStore[i].store(configJSONRef, logger, fileName, uploadedBy, cb);
+        } else {
+          logger.error(`Type of storageStore[i].store is ${typeof storageStore[i].store}`);
+          logger.error(`Its content is ${storageStore[i].store}`);
+        }
+      } else {
+        logger.error(`Type of storageStore[i] is ${typeof storageStore[i]}`);
+        logger.error(`Its content is ${storageStore[i]}`);
+      }
+    }
+  } else {
+    logger.error(`Type of storageStore is ${typeof storageStore}`);
+  }
+  logger.info(ret);
+  filesUploaded[fileId] = ret;
+  logger.info(filesUploaded[fileId]);
 }
-// returns IPFS cid
+// returns IPFS cid(s) as string or JSON array.
 
-tusServer.on(EVENTS.EVENT_UPLOAD_COMPLETE, async (event) => {
-  logger.info(`Receive complete for file ${event.file.id}`);
-  const uploadMetadata = tusMetadata.decode(event.file.upload_metadata);
-  filesUploaded[event.file.id].progress = 'uploading';
-  await uploadFile(configJSON, sanitize(event.file.id), uploadMetadata.username);
-});
-
-tusServer.on(EVENTS.EVENT_ENDPOINT_CREATED, async (event) => {
-  const id = await event.url.substring(event.url.lastIndexOf('/') + 1);
-  filesUploaded[id].progress = 'waiting';
-  logger.info(`Endpoint created with id ${id}`);
-});
-
-tusServer.on(EVENTS.EVENT_FILE_CREATED, async (event) => {
-  const { id } = event.file;
-  filesUploaded[event.file.id] = new Map();
-  logger.info(`File created with id ${id}`);
-  filesUploaded[event.file.id].progress = 'receiving';
-});
+function saveUploadData(fileID, data, safe = true) {
+  return new Promise((resolve, reject) => {
+    if (!safe || typeof filesUploaded[fileID] === 'undefined') {
+      filesUploaded[fileID] = data;
+      resolve(true);
+    } else {
+      reject(new Error('Unable to overwrite upload data safely.'));
+    }
+  });
+}
 
 // const app = express();
-async function authenticateRequest(req, res, next) {
-  let signature = Buffer.from(`${req.headers.signature}`, 'base64url').toString('utf8');
-
-  if (typeof signature === 'string') {
-    try {
-      signature = await JSON.parse(signature);
-    } catch (e) {
-      logger.warn(e);
-    }
-  } else {
-    logger.warn(`Type of signature= ${typeof signature}`);
-  }
-  // let signature = req.headers.signature;
-  let r;
-  logger.debug(signature);
-  if (['POST', 'PATCH', 'GET', 'HEAD'].includes(req.method) && typeof signature.username !== 'undefined' && typeof signature.ts !== 'undefined' && typeof req.headers.signature !== 'undefined' && typeof signature.pubkey !== 'undefined') {
-    const { ts } = signature;
-    if (ts > (Date.now() - 3600000)) {
-      try {
-        r = javalon.signVerify(signature, signature.username, 3600000);
-        logger.debug('Got correct signature.');
-        if (typeof next === 'function') next(req, res);
-        else r = true;
-      } catch (reason) {
-        logger.debug('Invalid signature');
-        logger.debug(reason);
-        res.send({ status: 'error', error: 'Invalid signature!' });
-      }
-    } else if (typeof ts === 'number') {
-      res.send({ status: 'error', error: 'Timestamp expired.' });
-      logger.debug('Authentication not valid, timestamp expired!');
-    } else if (typeof ts !== 'number') {
-      res.send({
-        status: 'error',
-        error: 'Invalid timestamp.',
-        type: typeof ts,
-        value: signature.ts,
-      });
-      logger.debug(typeof ts);
-    } else {
-      logger.debug('Authentication not valid, unknown error!');
-    }
-  } else if (typeof signature.username === 'undefined' && ['POST', 'PATCH', 'HEAD'].includes(req.method)) {
-    logger.debug('Username not specified!');
-    res.send({ status: 'error', error: 'Username not specified!' });
-  } else if (req.method === 'GET' && typeof signature.username === 'undefined') {
-    // logger.info('Got appInfo request...');
-    // res.send({ version, app: 'dtube-web3storage-uploader' });
-    // r = true;
-  } else {
-    logger.warn('Auth not valid!');
-    logger.warn(`Req method: ${req.method}`);
-    logger.warn(`Req headers: ${req.headers.toString()}`);
-  }
-  return r;
-}
 
 const uploadApp = express();
-const authApp = express();
+uploadApp.use(express.json());
 
-uploadApp.all('*', (req, res) => tusServer.handle(req, res).catch((reason) => {
-  logger.warn(reason);
-}));
-
-authApp.get('/progress/:token', (req, res) => {
+uploadApp.get('/progress/:token', async (req, res) => {
   const { token } = req.params;
-  logger.debug(token);
-  if (typeof filesUploaded[token] !== 'undefined' && filesUploaded[token].progress !== null) {
-    if (filesUploaded[token].progress === 'received') {
-      filesUploaded[token].progress = 'uploading';
-      res.send(filesUploaded[token]);
-    } else if (filesUploaded[token].progress === 'uploaded') {
+  // logger.debug(token);
+  if (typeof filesUploaded[token] !== 'undefined' && typeof filesUploaded[token].progress !== 'undefined') {
+    if (filesUploaded[token].progress === 'uploaded') {
       const tmp = filesUploaded[token];
       delete filesUploaded[token];
       res.send(tmp);
     } else {
       res.send(filesUploaded[token]);
     }
-  } else {
-    res.send({ status: 'error', error: 'Token not found.' });
+  } else if (typeof filesUploaded[token] === 'undefined') {
+    res.send({ status: 'unknown' });
   }
 });
 
-authApp.get('/version', (req, res) => res.send({ version, app: 'dtube-web3storage-uploader' }).catch((reason) => logger.warn(reason)));
-
-const authUpload = (req, res) => {
-  if (authenticateRequest(req, res) || true) { // bypass authentication, as it is not complete rn.
-    uploadApp(req, res);
-  } else {
-    logger.debug('Request not authed!');
-  }
-};
-
-authApp.get('/upload', authUpload);
-authApp.post('/upload', authUpload);
-authApp.head('/upload', authUpload);
-authApp.patch('/upload', authUpload);
-
-authApp.get('/download/:filename', (req, res) => {
-  const { filename } = req.params;
-  logger.debug(filename);
-  if (fs.existsSync(`./files/${filename}`)) {
-    const mimetype = mime.lookup(`./files/${filename}`);
-    logger.debug(filename);
-    res.setHeader('Content-disposition', `attachment; filename=${filename}`);
-    res.setHeader('Content-type', mimetype);
-    logger.debug(mimetype);
-    const fileContent = fs.readFileSync(`./files/${filename}`);
-    res.send(fileContent);
-  } else {
-    logger.debug(`${filename} not found!`);
-    res.status(404).send('File not found');
-  }
-});
+uploadApp.get('/version', (req, res) => res.send({ version, app: 'dtube-web3storage-uploader' }));
 
 if (opts.daemon) {
-  https.createServer(
-    // Provide the private and public key to the server by reading each
-    // file's content with the readFileSync() method.
-    {
-      key: fs.readFileSync(`${configJSON.cert_directory}/privkey.pem`),
-      cert: fs.readFileSync(`${configJSON.cert_directory}/cert.pem`),
-      ca: fs.readFileSync('./certs/3334561879.crt'),
-    },
-    authApp,
-  ).listen(port, '0.0.0.0', () => { logger.info(`TUS Auth listening on port ${port}`); });
+  uploadApp.post('/hooks', async (req, res) => {
+    const upload = req.body.Upload;
+    if (req.headers['hook-name'] === 'post-finish') {
+      logger.info(`Receive complete for file ${upload.ID}`);
+      await saveUploadData(upload.ID, { progress: 'uploading' }, false).then(async () => {
+        await uploadFile(configJSON, upload.ID, upload.MetaData.username, (res2) => {
+          if (res2 !== false) {
+            let ret = {};
+            if (typeof res2 === 'string') {
+              ret.cid = res2;
+              ret.progress = 'uploaded';
+            } else if (typeof res2 === 'object') {
+              if (res2.length === 1) {
+                ret.cid = res2[0];
+                ret.progress = 'uploaded';
+              } else if (res2.length > 1) {
+                ret.cid = res2[randomInt(res2.length)];
+                ret.cid_list = res2;
+                ret.progress = 'uploaded';
+              }
+            }
+            saveUploadData(upload.ID, ret, false);
+          }
+          fs.unlink(`./files/${upload.ID}`, (err) => {
+            if (err) {
+              logger.error(`Error deleting file ${upload.ID}!`);
+              logger.error(err);
+            } else {
+              logger.debug(`File ${upload.ID} deleted!`);
+            }
+          });
+        });
+      });
+    } else if (req.headers['hook-name'] === 'post-create') {
+      await saveUploadData(upload.ID, { progress: 'receiving' });
+    } else if (req.headers['hook-name'] === 'post-terminate') {
+      logger.info('Upload terminated early!');
+      fs.unlink(`./files/${upload.ID}`, (err) => {
+        if (err) {
+          logger.error(`Error deleting file ${upload.ID}!`);
+          logger.error(err);
+        } else {
+          logger.debug(`File ${upload.ID} deleted!`);
+        }
+      });
+    }
+  });
+  uploadApp.listen(port);
+  logger.info(`Listening on port ${port}`);
 }
---
+++ proxy.mjs
@@ -0,0 +1,181 @@
+import net from 'net';
+import log4js from 'log4js';
+import util from 'util';
+import javalon from './javalon.js';
+
+log4js.configure({
+  appenders: {
+    logs: { type: 'file', filename: 'logs/logs.log' },
+    console: { type: 'console' },
+  },
+  categories: {
+    logs: { appenders: ['logs'], level: 'trace' },
+    console: { appenders: ['console'], level: 'trace' },
+    default: { appenders: ['console', 'logs'], level: 'info' },
+  },
+});
+
+const port = 5081;
+
+javalon.init({ api: 'https://dtube.fso.ovh' });
+const LOG_LEVEL = 'debug';
+const logger = log4js.getLogger();
+logger.level = LOG_LEVEL;
+
+async function authenticateRequest(base64signature) {
+  logger.debug(base64signature);
+  let signature = Buffer.from(base64signature, 'base64').toString('utf8');
+  if (typeof signature === 'string') {
+    try {
+      signature = JSON.parse(signature);
+    } catch (e) {
+      logger.warn(e);
+    }
+  } else {
+    logger.warn(`Type of signature= ${typeof signature}`);
+  }
+  // let signature = req.headers.signature;
+  let r;
+  let ownedKey;
+  logger.debug(signature);
+  if (typeof signature.username !== 'undefined' && typeof signature.ts !== 'undefined' && typeof signature.pubkey !== 'undefined') {
+    await util.promisify(
+      javalon.getAccount,
+    )(signature.username).then((account) => {
+      if (typeof account !== 'undefined') {
+        for (let i = 0; i < account.keys.length; i += 1) {
+          if (account.keys[i].pub === signature.pubkey) {
+            ownedKey = true;
+          }
+        }
+      }
+      return false;
+    }).catch((err) => {
+      logger.error(err);
+      return false;
+    });
+
+    const { ts } = signature;
+    if (ts > (Date.now() - 3600000) && ownedKey) {
+      try {
+        r = javalon.signVerify(signature, signature.username, 3600000);
+        if (r) {
+          logger.debug('Got correct signature.');
+          r = true;
+        } else {
+          r = false;
+        }
+      } catch (reason) {
+        logger.debug('Invalid signature');
+        logger.debug(reason);
+        r = { status: 'error', error: 'Invalid signature!' };
+      }
+    } else if (!ownedKey) {
+      logger.debug('Key not owned by user on chain!');
+    } else if (typeof ts === 'number') {
+      r = { status: 'error', error: 'Timestamp expired.' };
+      logger.debug('Authentication not valid, timestamp expired!');
+    } else if (typeof ts !== 'number') {
+      r = {
+        status: 'error',
+        error: 'Invalid timestamp.',
+        type: typeof ts,
+        value: signature.ts,
+      };
+      logger.debug(typeof ts);
+    } else {
+      logger.debug('Authentication not valid, unknown error!');
+    }
+  } else {
+    r = false;
+    logger.warn('Auth not valid!');
+  }
+  return r;
+}
+
+const server = net.createServer();
+
+server.on('connection', (clientToProxySocket) => {
+  logger.debug('Client connected to proxy');
+  let r;
+  clientToProxySocket.once('data', async (data) => {
+    let needsAuth = true;
+    let isTLSConnection = data.toString().indexOf('CONNECT') !== -1;
+    isTLSConnection = false;
+    const serverAddress = '127.0.0.1';
+    const serverPort = 5083;
+    // logger.debug(data.toString());
+    logger.debug(serverAddress);
+    if ((typeof r === 'undefined' || r === false) && needsAuth) {
+      try {
+        r = await authenticateRequest(data.toString().split('signature: ')[1].split('\r\n')[0]);
+      } catch (e) {
+        logger.debug(e);
+        r = false;
+      }
+    }
+    if ((r !== false && needsAuth) || !needsAuth) {
+      logger.debug(r);
+      // Creating a connection from proxy to destination server
+      let proxyToServerSocket = net.createConnection(
+        {
+          host: serverAddress,
+          port: serverPort,
+        },
+        () => {
+          logger.debug('Proxy to server set up');
+        },
+      );
+
+      if (isTLSConnection) {
+        clientToProxySocket.write('HTTP/1.1 200 OK\r\n\r\n');
+      } else {
+        proxyToServerSocket.write(data);
+      }
+
+      clientToProxySocket.pipe(proxyToServerSocket);
+      proxyToServerSocket.pipe(clientToProxySocket);
+
+      proxyToServerSocket.on('error', (err) => {
+        logger.error('Proxy to server error');
+        logger.error(err);
+        clientToProxySocket.write('HTTP/1.1 502 Bad Gateway\r\n\r\nBad Gateway\r\n\r\n');
+        clientToProxySocket.end();
+      });
+      proxyToServerSocket.on('close', () => {
+        logger.info('Server closed connection');
+      });
+      clientToProxySocket.on('error', (err) => {
+        logger.error('Client to proxy error');
+        logger.error(err);
+        r = undefined;
+      });
+      clientToProxySocket.on('close', () => {
+        logger.info('Client closed connection');
+        r = undefined;
+      });
+    } else {
+      clientToProxySocket.write('HTTP/1.1 401 Invalid Authentication\r\n\r\nInvalid Authentication\r\n\r\n');
+      clientToProxySocket.end();
+    }
+  });
+});
+
+server.on('error', (err) => {
+  logger.error('Some internal server error occurred');
+  logger.error(err);
+});
+
+server.on('close', () => {
+  logger.debug('Client disconnected');
+});
+
+server.listen(
+  {
+    host: '0.0.0.0',
+    port: port,
+  },
+  () => {
+    logger.info(`Server listening on 0.0.0.0:${port}`);
+  },
+);
